bc4144d3bebce6a1e7f44a5111db79b98b8046a4
Ecko <ecko782@yandex.ru>
2020-02-03 16:35:39 +0700

    New

--- test1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-	int main() {
-		std::cout << "git test\n"; 
-		std::vector<int> vec = {1, 2, 3};
-		for (auto it : vec)
-			std::cout << it << "\n";
-		return 0;
-	}
